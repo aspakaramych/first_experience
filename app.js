1c1c3b057a4f42cbed24306eebc321fa28c28cd9
Vlad Grigoriev <vgrig2006@gmail.com>
2023-05-30 18:14:07 +0700

    add new fun

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const BodyParser = require('body-parser');
 const authRoutes = require('./routes/auth');
+const cors = require('cors');
+const morgan = require('morgan');
 const analyticsRoutes = require('./routes/analytics');
 const categoryRoutes = require('./routes/category');
 const orderRoutes = require('./routes/order');
 const positionRoutes = require('./routes/position');
 const app = express();
+
+app.use(morgan('dev'));
+app.use(BodyParser.urlencoded({extended: true}));
+app.use(BodyParser.json());
+app.use(cors());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/analytics', analyticsRoutes);
