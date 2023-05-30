9d45617650b3671c5a4ce7c774b727409da982e0
Vlad Grigoriev <vgrig2006@gmail.com>
2023-05-30 20:35:27 +0700

    add new fun

--- app.js
+++ app.js
@@ -3,11 +3,28 @@ const BodyParser = require('body-parser');
 const authRoutes = require('./routes/auth');
 const cors = require('cors');
 const morgan = require('morgan');
+const keys = require('./config/keys');
+const MongoClient = require("mongodb").MongoClient
 const analyticsRoutes = require('./routes/analytics');
 const categoryRoutes = require('./routes/category');
 const orderRoutes = require('./routes/order');
 const positionRoutes = require('./routes/position');
 const app = express();
+
+const mongoClient = new MongoClient(keys.Db_url)
+const db = mongoClient.db(keys.Db_locate)
+
+MongoClient.connect()
+    .then(() => {
+        console.log('MongoDb connected')
+        console.log(db.command({ping: 1}))
+    })
+    .catch(err => console.log(err))
+    .finally(() => {
+        MongoClient.close()
+        console.log('MongoDb disconnected')
+    })
+
 
 app.use(morgan('dev'));
 app.use(BodyParser.urlencoded({extended: true}));
---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    Db_url: 'mongodb://127.0.0.1:27017/',
+    Db_locate: './database/identifier'
+}
